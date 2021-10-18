f4838ea0632ab168df5101a6eda15c71be83e1a7
vishal.Alok <you@example.com>
2021-10-18 08:06:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void AreaDelegate(double x, double y);
+    class Rectangle
+    {
+        public void GetArea(double width, double height)
+        {
+            Console.WriteLine("Area of rectangle" + width * height);
+        }
+        static void Main(string[] args)
+        {
+            Rectangle p = new Rectangle();
+            AreaDelegate obj = delegate (double width, double height)
+            {
+                Console.WriteLine("Area of rectangle " + width * height);
+            };
+            obj.Invoke(12, 43);
+        }
+    }
+}
